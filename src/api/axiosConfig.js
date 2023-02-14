c92d8012d6eb5fb2768aa0c9fa44fede22cc5ee8
manas-maheshwari <maheshwarimanas.07@gmail.com>
2023-02-13 23:12:47 -0600

    update the trailer component and add play funcyionality

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://8732-2603-8081-3100-fb-e1ec-130e-c70b-6c29.ngrok.io',
+    baseURL:'https://678d-2603-8081-3100-fb-a0ec-79c3-b3e-3bcd.ngrok.io',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
--- src/components/trailer/Trailer.js
+++ src/components/trailer/Trailer.js
@@ -4,8 +4,22 @@ import './Trailer.css';
 
 
 const Trailer = () => {
+
+  let params = useParams();
+  const key = params.ytTrailerId;
+
+
+
+
   return (
-    <div>Trailer</div>
+    <div className="react-player-container">
+
+      {(key!=null)?<ReactPlayer controls="true" playing={true} url={`https://www.youtube.com/watch?v=${key}`}
+      width="100%"
+      height='100%'
+      /> : null}
+
+    </div>
   )
 }
 
